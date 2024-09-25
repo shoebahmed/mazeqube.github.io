210015a333197cb67e085bf726a7aaf23ed8476e
shoebahmed <shoebahmed@yahoo.com>
2024-09-26 02:21:47 +0530

    Header dynamically

--- js/header.js
+++ js/header.js
@@ -1,22 +1,20 @@
 // JavaScript to load header.html into the page
 document.addEventListener("DOMContentLoaded", function() {
-  fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header-placeholder').innerHTML = data;
 
-      // Select all <a> elements in the newly loaded header
-      const links = document.querySelectorAll('#header-placeholder a');
-
-
-      // Loop through each <a> and replace ".html" with "" (remove extension)
-      links.forEach(link => {
-        if(link.href.indexOf('index.html') > 1) {
-          link.href = link.href.replace('index.html', '');
-        } else {
-          link.href = link.href.replace('.html', '');
-        }
-      });
-    })
-    .catch(error => console.error('Error loading the header:', error));
+    // Select all <a> elements in the newly loaded header
+    const firstLinks = document.querySelectorAll('#header-placeholder a');
+    processLinks(firstLinks);
+    const secondLinks = document.querySelectorAll('#header-industries a');
+    processLinks(secondLinks);
 });
+
+function processLinks(links) {
+  // Loop through each <a> and replace ".html" with "" (remove extension)
+  links.forEach(link => {
+    if(link.href.indexOf('index.html') > 1) {
+      link.href = link.href.replace('index.html', '');
+    } else {
+      link.href = link.href.replace('.html', '');
+    }
+  });
+}
